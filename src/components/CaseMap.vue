19bcfc45586d96989216004eefd66ebacada2c14
xdoo <claus.straube@gmail.com>
2019-02-12 13:55:31 +0100

    added router and vuex

---
+++ src/components/CaseMap.vue
@@ -0,0 +1,13 @@
+<template>
+  <l-map ref="myMap"></l-map>
+</template>
+<script>
+export default {
+  mounted () {
+      this.$nextTick(() => {
+        this.$refs.myMap.mapObject.ANY_LEAFLET_MAP_METHOD();
+      })
+    }
+}
+</script>
+
